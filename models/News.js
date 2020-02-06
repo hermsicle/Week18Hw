abf1802e713b5d34d5643af5b723a8a4ae458b85
Herman Liu <hermanliu95@gmail.com>
2020-02-06 08:32:35 -0800

    added mongoose Schema

---
+++ models/News.js
@@ -0,0 +1,26 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NewsSchema = new Schema({
+    headline: {
+        type: String,
+        trim: true,
+        require: "Must pass a string value for headline"
+    },
+
+    text: {
+        type: String,
+        required: "Must pass a string value for headline"
+    },
+
+    comments: {
+        type: [Schema.Types.String],
+        default: [],
+        required: "Must pass a string value for headline"
+    }
+})
+
+const News = mongoose.model("News", NewsSchema);
+
+module.exports = News;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = 3000 || process.env.PORT;
+const connection = mongoose.connection;
+const colors = require('colors');
+const PORT = 5000 || process.env.PORT;
 
-app.use(express.urlencoded({ extended: true });
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+connection.once("open", () => {
+    console.log('connected to mongoDb'.cyan); //Cyan is colors
+    console.log('-------------------------------'.rainbow) //Rainbow is cyan
+})
+
 app.listen(PORT, () => {
+    console.log('------------------------------------'.rainbow)
     console.log(`Listening to http://localhost:${PORT}`)
 })
