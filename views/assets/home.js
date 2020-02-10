a35bff7d814386e0cc3750c2859ed4b6a7cd8157
Herman Liu <hermanliu95@gmail.com>
2020-02-10 00:06:21 -0800

    header added in

--- views/assets/home.js
+++ views/assets/home.js
@@ -1,16 +1,1 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-
-axios.get("https://www.awwwards.com/websites/clean/").then(urlResponse => {
-    let $ = cheerio.load(urlResponse.data);
-    $("figure.rollover").each((i, element) => {
-        const imgLink = $(element)
-            .find("img")
-            .attr("data-srcset")
-            .split(", ")[1]
-            .split(" ")[0];
-        console.log(imgLink, "\n");
-    });
-});
-
+console.log("hello worlds")
